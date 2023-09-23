66766dc8834590dcdb9aa9a6ab8c869b5caac2f6
RoboticsDevelopmentName <RoboticsDevelopment@protonmail.com>
2023-09-23 09:37:23 -0400

    Toggle Instructor

--- src/ClassPage.jsx
+++ src/ClassPage.jsx
@@ -1,34 +1,42 @@
 import React from "react";
 import { getRandomUser } from "./Utility/api";
+import Instructor from "./Instructor";
 
 export default class ClassPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.state = JSON.parse(localStorage.getItem("classStateLocalStorage")) || {
       instructor: undefined,
       studentList: [],
       studentCount: 0,
       hideInstructor: false,
+      inputName: "",
+      inputFeedback: "",
     };
   }
 
   componentDidMount = async () => {
     console.log("Component Did Mount");
-    const response = await getRandomUser();
-    console.log(response);
-    this.setState((prevState) => {
-      return {
-        instructor: {
-          name: response.data.first_name + " " + response.data.last_name,
-          email: response.data.email,
-          phone: response.data.phone_number,
-        },
-      };
-    });
+    if (JSON.parse(localStorage.getItem("classStateLocalStorage"))) {
+      this.setState(JSON.parse(localStorage.getItem("classStateLocalStorage")));
+    } else {
+      const response = await getRandomUser();
+      console.log(response);
+      this.setState((prevState) => {
+        return {
+          instructor: {
+            name: response.data.first_name + " " + response.data.last_name,
+            email: response.data.email,
+            phone: response.data.phone_number,
+          },
+        };
+      });
+    }
   };
 
   componentDidUpdate() {
     console.log("Component Did Update");
+    localStorage.setItem("classStateLocalStorage", JSON.stringify(this.state));
   }
 
   componentWillUnmount() {
@@ -51,19 +59,53 @@ export default class ClassPage extends React.Component {
     });
   };
 
+  handleToggleInstructor = () => {
+    this.setState((prevState) => {
+      return {
+        hideInstructor: !prevState.hideInstructor,
+      };
+    });
+  };
+
   render() {
     return (
       <div>
-        {this.state.instructor && (
-          <div className="p-3">
-            <span className=" h4 text-success">Instructor</span>
-            <i className="bi bi-toggle-off btn btn-success btn-sm"></i>
-            <br />
-            Name: {this.state.instructor.name} <br />
-            Email: {this.state.instructor.email} <br />
-            Phone: {this.state.instructor.phone}
-          </div>
-        )}
+        <div className="p-3">
+          <span className=" h4 text-success">Instructor &nbsp;</span>
+          <i
+            className={`bi ${
+              this.state.hideInstructor ? "bi-toggle-off" : "bi-toggle-on"
+            } btn btn-success btn-sm`}
+            onClick={this.handleToggleInstructor}
+          ></i>
+
+          {!this.state.hideInstructor ? (
+            <Instructor instructor={this.state.instructor} />
+          ) : null}
+        </div>
+
+        <div className="p-3">
+          <span className="h4 text-success">Feedback</span>
+          <br />
+          <input
+            type="text"
+            value={this.state.inputName}
+            placeholder="Name.."
+            onChange={(e) => {
+              this.setState({ inputName: e.target.value });
+            }}
+          ></input>
+          Value: {this.state.inputName}
+          <br />
+          <textarea
+            value={this.state.inputFeedback}
+            onChange={(e) => {
+              this.setState({ inputFeedback: e.target.value });
+            }}
+            placeholder="Feedback..."
+          ></textarea>
+          Value: {this.state.inputFeedback}
+        </div>
         <div className="p-3">
           <span className="h4 text-success">Students</span> <br />
           <div>Student Count: {this.state.studentCount}</div>
